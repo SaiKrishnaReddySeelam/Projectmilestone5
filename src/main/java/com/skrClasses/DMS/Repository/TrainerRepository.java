7cf69897e5addb2a3bb41ff90c8c2ef3738a5adb
Sai Krishna Reddy Seelam <143019716+Skr299@users.noreply.github.com>
2024-04-17 17:25:12 -0500

    Changes

---
+++ src/main/java/com/skrClasses/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.skrClasses.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.skrClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
